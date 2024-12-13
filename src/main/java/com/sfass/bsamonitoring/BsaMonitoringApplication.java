7de0fc0c4f30cedbd1e2b78151072cb69651e456
kgw100 <kgw100@yonsei.ac.kr>
2024-12-13 11:48:43 +0900

    0.0 origin version

---
+++ src/main/java/com/sfass/bsamonitoring/BsaMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.sfass.bsamonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsaMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sfass/bsamonitoring/BsaMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfass.bsamonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsaMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
