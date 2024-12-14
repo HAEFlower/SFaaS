6e92f58f0cf239978f9940cb839d9ef26adec1a2
idealflower-k <aekrcu@naver.com>
2024-12-14 19:11:58 +0900

    feat/ 에러응답 구조 추가

--- src/main/java/com/sfass/bsamonitoring/global/GlobalResponse.java
+++ src/main/java/com/sfass/bsamonitoring/global/GlobalResponse.java
@@ -2,12 +2,14 @@ package com.sfass.bsamonitoring.global;
 
 import java.time.LocalDateTime;
 
+import com.sfass.bsamonitoring.global.error.ErrorResponse;
+
 public record GlobalResponse(boolean status, Object data, LocalDateTime timestamp) {
 	public static GlobalResponse ok(Object data) {
 		return new GlobalResponse(true, data, LocalDateTime.now());
 	}
 
-	public static GlobalResponse error(String message) {
-		return new GlobalResponse(false, message, LocalDateTime.now());
+	public static GlobalResponse error(ErrorResponse errorResponse) {
+		return new GlobalResponse(false, errorResponse, LocalDateTime.now());
 	}
 }
