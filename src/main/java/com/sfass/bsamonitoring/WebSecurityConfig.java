bd7328513f214bfc3d7c54db7241de02c7f7271f
idealflower-k <aekrcu@naver.com>
2024-12-16 14:37:49 +0900

    chore/ change set for dev

--- src/main/java/com/sfass/bsamonitoring/WebSecurityConfig.java
+++ src/main/java/com/sfass/bsamonitoring/WebSecurityConfig.java
@@ -5,14 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
         // CORS 비활성화
-        http.cors(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(request ->
+        {
+            var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+            corsConfiguration.addAllowedOriginPattern("*");
+            corsConfiguration.addAllowedMethod("*");
+            corsConfiguration.addAllowedHeader("*");
+            corsConfiguration.setAllowCredentials(true);
+            return corsConfiguration;
+        }));
 
         // CSRF 비활성화
         http.csrf(AbstractHttpConfigurer::disable);
