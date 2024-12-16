414f68ea696ab6c369c35ab6dc468abed687fd1f
kgw100 <kgw100@yonsei.ac.kr>
2024-12-16 18:42:27 +0900

    Fix: mapper build error

--- src/main/java/com/sfass/bsamonitoring/inventory/mapper/InventoryMapper.java
+++ src/main/java/com/sfass/bsamonitoring/inventory/mapper/InventoryMapper.java
@@ -3,12 +3,10 @@ package com.sfass.bsamonitoring.inventory.mapper;
 import com.sfass.bsamonitoring.inventory.dto.request.InventorySearchDTO;
 import com.sfass.bsamonitoring.inventory.dto.response.InventoryDTO;
 import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
 @Mapper
 public interface InventoryMapper {
     List<InventoryDTO> selectInventoryItems(InventorySearchDTO searchDTO);
-
     Long countInventoryItems(InventorySearchDTO searchDTO);
 }
