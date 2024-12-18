ec491c59a088e797895c5bdba3301515ac46c096
idealflower-k <aekrcu@naver.com>
2024-12-18 18:31:57 +0900

    feat/ 사원 삭제 추가

--- src/main/java/com/sfass/bsamonitoring/global/common/userConstants/UserConstants.java
+++ src/main/java/com/sfass/bsamonitoring/global/common/userConstants/UserConstants.java
@@ -3,4 +3,6 @@ package com.sfass.bsamonitoring.global.common.userConstants;
 public class UserConstants {
 	public static final String USER_AUTH_UPDATE_SUCCESS = "권한 변경이 완료되었습니다.";
 	public static final String USER_AUTH_UPDATE_FAIL = "변경 권한이 없습니다.";
+	public static final String USER_DELETE_SUCCESS = "사용자를 삭제하였습니다.";
+	public static final String USER_DELETE_FAIL = "삭제 권한이 없습니다.";
 }
