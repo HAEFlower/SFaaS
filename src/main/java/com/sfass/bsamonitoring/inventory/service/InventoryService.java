f1b3235efb1bb97bc30541cd2499597577bd67e2
kgw100 <kgw100@yonsei.ac.kr>
2024-12-18 19:21:52 +0900

    Fix: transaction orders and items

--- src/main/java/com/sfass/bsamonitoring/inventory/service/InventoryService.java
+++ src/main/java/com/sfass/bsamonitoring/inventory/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.sfass.bsamonitoring.inventory.service;
 import com.sfass.bsamonitoring.inventory.dto.request.InventorySearchDTO;
 import com.sfass.bsamonitoring.inventory.dto.response.InventoryDTO;
 import com.sfass.bsamonitoring.inventory.mapper.InventoryMapper;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,11 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+
 public class InventoryService {
 
     private final InventoryMapper inventoryMapper;
-
+    @Transactional(readOnly = true)
     public List<InventoryDTO> getInventoryItems(InventorySearchDTO searchDTO) {
         log.debug("Fetching inventory items with criteria: {}", searchDTO);
         return inventoryMapper.selectInventoryItems(searchDTO);
